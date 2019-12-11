1eaa8db24b05c1bd5e1c2e85e3d15bc6f611465b
aleksej <al1111997@yandex.ru>
2019-12-11 18:06:50 +0500

    add react navigation

---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,23 @@
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import {Platform} from 'react-native'
+import { MainScreen } from '../screens/MainScreen'
+import { PostScreen } from '../screens/PostScreen';
+import { THEME } from '../theme';
+
+const PostNavigator = createStackNavigator({
+    Main: MainScreen,
+    Post: {
+        screen: PostScreen
+    }
+}, {
+    initialRouteName: 'Main',
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS=== 'android' ? THEME.MAIN_COLOR : '#ffffff'
+        },
+        headerTintColor: Platform.OS=== 'android' ? '#ffffff' : THEME.MAIN_COLOR
+    }
+})
+
+export const AppNavigation = createAppContainer(PostNavigator)
